8beb059737baec41f38a90ccde8dc1f5c46cf8be
prakharpks02 <prakharpks02@gmail.com>
2018-10-26 23:26:48 +0530

    both app urls changed

--- Manager/urls.py
+++ Manager/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
-from django.urls import path, include
-from django.conf.urls import url
+#from django.urls import include
+from django.conf.urls import url, include
 from . import views
 
 urlpatterns = [
